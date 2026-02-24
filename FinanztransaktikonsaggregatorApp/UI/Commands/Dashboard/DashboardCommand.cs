506508c59af914a210999fbb634c3f9d78bb1d37
lucariebel <luca-riebel@outlook.com>
2026-02-24 09:24:55 +0100

    edit Dashboard

--- FinanztransaktikonsaggregatorApp/UI/Commands/Dashboard/DashboardCommand.cs
+++ FinanztransaktikonsaggregatorApp/UI/Commands/Dashboard/DashboardCommand.cs
@@ -9,12 +9,17 @@ public class DashboardCommand : ICommand
     {
         MenuHelper.CreateHeader("FINANCIAL DASHBOARD");
         Console.WriteLine();
+        Console.WriteLine($"Month: {DateTime.Now:MMMM}");
         Console.WriteLine("Total Transactions: 10");
         Console.WriteLine();
         Console.WriteLine("[ BALANCE OVERVIEW ]");
-        Console.WriteLine("Current Balance: 100€");
-        Console.WriteLine("Total Income:    + 1000€");
-        Console.WriteLine("Total Expenses:  - 900");
+        Console.WriteLine($"Current Balance: {100,15:C2}");
+        Console.WriteLine($"Total Income:    + {1000,15:C2}");
+        Console.WriteLine($"Total Expenses:  - {900,15:C2}");
+        MenuHelper.CreateHorizontalLine();
+        Console.WriteLine($"Net Savings:      {(1000-900),15:C2}");
+        Console.WriteLine();
+        Console.WriteLine("[ TOP 3 EXPENSES ]");
         Console.WriteLine();
         Console.WriteLine("Press key to return");
         Console.ReadKey();
