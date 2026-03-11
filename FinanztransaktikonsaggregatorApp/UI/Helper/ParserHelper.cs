29f51a7beea883268a819698a1055387af0d4c57
Christoph <chrisi.niederer@gmail.com>
2026-03-11 11:28:05 +0100

    add ParserHelper class

---
+++ FinanztransaktikonsaggregatorApp/UI/Helper/ParserHelper.cs
@@ -0,0 +1,15 @@
+namespace FinanztransaktikonsaggregatorApp.Controllers.Helper;
+
+public static class ParserHelper
+{
+    public static int ParseInteger(string input)
+    {
+        int number;
+        while (!int.TryParse(input, out number) || number <= 0)
+        {
+            Console.WriteLine("Invalid number. Please enter a valid positive id:");
+            input = Console.ReadLine();
+        }
+        return number;
+    }
+}
