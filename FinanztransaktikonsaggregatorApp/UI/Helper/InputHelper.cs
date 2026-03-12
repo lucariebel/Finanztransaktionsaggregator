82b19308f8d374b2a43ebc54c86ee0ec73f60533
Christoph <chrisi.niederer@gmail.com>
2026-03-12 12:33:40 +0100

    add InputHelper class
    - add GetRquiredString method
    - add GetValidId method
    - add GetRequiredDecimal method
    sh: line 1: q: command not found

---
+++ FinanztransaktikonsaggregatorApp/UI/Helper/InputHelper.cs
@@ -0,0 +1,44 @@
+﻿
+namespace FinanztransaktikonsaggregatorApp.Controllers.Helper;
+
+public static class InputHelper
+{
+    public static string GetRequiredString(string message)
+    {
+        string input = "";
+
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine(message);
+            input = Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    public static int GetValidId(string message, Func<int, bool> validator,string input)
+    {
+        int id = ParserHelper.ParseInteger(input);
+
+        while (!validator(id))
+        {
+            Console.WriteLine(message);
+            input = Console.ReadLine();
+            id = ParserHelper.ParseInteger(input);
+        }
+
+        return id;
+    }
+
+    public static decimal GetRequiredDecimal(string message)
+    {
+        string input = "";
+
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine(message);
+            input = Console.ReadLine();
+        }
+        return ParserHelper.ParseDecimal(input);
+    }
+}
