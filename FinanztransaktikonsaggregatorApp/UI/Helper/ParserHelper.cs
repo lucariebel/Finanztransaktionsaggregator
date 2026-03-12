df1adfd5579b99d0791c362d33b47e1423f52f54
Christoph <chrisi.niederer@gmail.com>
2026-03-12 12:39:12 +0100

    add ParseDecimal method

--- FinanztransaktikonsaggregatorApp/UI/Helper/ParserHelper.cs
+++ FinanztransaktikonsaggregatorApp/UI/Helper/ParserHelper.cs
@@ -12,4 +12,15 @@ public static class ParserHelper
         }
         return number;
     }
+
+    public static decimal ParseDecimal(string input)
+    {
+        decimal number;
+        while (!decimal.TryParse(input, out number) || number <= 0)
+        {
+            Console.WriteLine("Invalid number. Please enter a valid positive id:");
+            input = Console.ReadLine();
+        }
+        return number;
+    }
 }
